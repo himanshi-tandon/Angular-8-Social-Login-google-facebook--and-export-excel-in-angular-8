662855d67f47622bbde565b5a05a27f4fc624a3b
Himanshi Tandon <Himanshi.Tandon@igglobal.com>
2020-05-20 17:37:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as xlsx from 'xlsx';
 import {
   AuthService,
   SocialUser,
@@ -13,9 +13,56 @@ import {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('epltable', { static: false }) epltable: ElementRef;
   public user: SocialUser;
   public authorized: boolean = false;
-  
+  clubs = [
+    {
+      position: 1,
+      name: "Liverpool",
+      played: 20,
+      won: 19,
+      drawn: 1,
+      lost: 0,
+      points: 58
+    },
+    {
+      position: 2,
+      name: "Leicester City",
+      played: 21,
+      won: 14,
+      drawn: 3,
+      lost: 4,
+      points: 45
+    },
+    {
+      position: 3,
+      name: "Manchester City",
+      played: 21,
+      won: 14,
+      drawn: 2,
+      lost: 5,
+      points: 44
+    },
+    {
+      position: 4,
+      name: "Chelsea",
+      played: 21,
+      won: 11,
+      drawn: 3,
+      lost: 7,
+      points: 36
+    },
+    {
+      position: 5,
+      name: "Manchester United",
+      played: 21,
+      won: 8,
+      drawn: 7,
+      lost: 6,
+      points: 31
+    }
+   ];
 
 
   constructor( private socialAuthService: AuthService ) {}
@@ -45,6 +92,16 @@ export class AppComponent {
           this.socialAuthService.signOut();
           this.authorized = false;
       }
+
+
+      exportToExcel() {
+        const ws: xlsx.WorkSheet =   
+        xlsx.utils.table_to_sheet(this.epltable.nativeElement);
+        const wb: xlsx.WorkBook = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(wb, ws, 'Sheet1');
+        xlsx.writeFile(wb, 'epltable.xlsx');
+       }
+      }
   
-}
+
 
